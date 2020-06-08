c6cee9d75b7eb80c7fc0b4f92b6a0c3a00867ac4
Guilherme Zago <contatogzii@gmail.com>
2020-06-08 12:58:22 -0300

    Updates

---
+++ precache-manifest.87c82bc183c53bf03729e560a71ccb93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4324e74545ad3d04c8fe2d72f2791a",
+    "url": "/codinome007/index.html"
+  },
+  {
+    "revision": "8925441cace8255744be",
+    "url": "/codinome007/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bf75596a79caac91abd",
+    "url": "/codinome007/static/js/2.ef402e00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codinome007/static/js/2.ef402e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8925441cace8255744be",
+    "url": "/codinome007/static/js/main.dcf6b97c.chunk.js"
+  },
+  {
+    "revision": "63fe773ac9d94a2506d7",
+    "url": "/codinome007/static/js/runtime-main.d8b27cb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/codinome007/static/media/logo.5d5d9eef.svg"
+  }
+]);
